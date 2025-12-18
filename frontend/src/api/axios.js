7d9fdda546daa983256113a8d0d9bd339e12cfed
pranay1303 <150463014+pranay1303@users.noreply.github.com>
2025-12-18 17:45:22 +0530

    configure frontend for vercel backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,7 @@
-// src/api/axios.js
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000";
-
 const api = axios.create({
-  baseURL,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: { "Content-Type": "application/json" },
 });
 
